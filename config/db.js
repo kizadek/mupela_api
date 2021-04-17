11d82f534a9a0a3e472642cad9d1ff21e57cdfe1
Mupela <48857035+kizadek@users.noreply.github.com>
2021-04-17 21:47:47 +0200

    init new🍗🍖🍗

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ const colors = require('colors');
 const connectDB = async () =>{
     const conn = await mongoose.connect(process.env.MONGO_URL,{
         useNewUrlParser: true,
-        useUnifiedTopology:true,
-        useFindAndModify:false,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
         useCreateIndex: true
     })
     console.log(`MongoDB Connected ${ conn.connection.host}`.rainbow.bold)
---
+++ utils/geocoder.js
@@ -0,0 +1,16 @@
+const NodeGeocoder = require('node-geocoder');
+
+
+const options = {
+    provider: process.env.GEOCODER_PROVIDER,
+  
+    // Optionnal depending of the providers
+    httpAdapter: 'https', // Default
+    apiKey: process.env.GEOCODER_API_KEY, // for Mapquest, OpenCage, Google Premier
+    formatter: null         // 'gpx', 'string', ...
+  };
+  
+  const  geocoder = NodeGeocoder(options);
+
+
+  module.exports = geocoder;
